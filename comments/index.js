e30f0eae99fc47df837a3a7fe7bf311b2e43ad59
Justice Eziefule <thechiefje@gmail.com>
2021-01-14 14:02:13 -0800

    add query service

--- comments/index.js
+++ comments/index.js
@@ -39,6 +39,10 @@ app.post("/posts/:id/comments", async (req, res) => {
     res.status(201).send(comments)
 })
 
+app.post("/events", (req, res) => {
+    res.send({})
+})
+
 app.listen(4001, () => {
     console.log("Listening on port 4001")
 })
