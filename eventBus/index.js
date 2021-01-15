7f6a11e02493b51899dfa4b25d7acce3f7a9ed5f
Justice Eziefule <thechiefje@gmail.com>
2021-01-15 14:21:03 -0800

    sync events

--- eventBus/index.js
+++ eventBus/index.js
@@ -6,9 +6,11 @@ const axios = require("axios")
 const app = express();
 app.use(bodyParser.json())
 
+const events = [];
+
 app.post("/events", (req, res) => {
     const event = req.body;
-
+    events.push(event);
     axios.post("http://localhost:4000/events", event)
     axios.post("http://localhost:4001/events", event)
     axios.post("http://localhost:4002/events", event)
@@ -16,8 +18,12 @@ app.post("/events", (req, res) => {
 
     res.send({status: "OK"})
 
-})
+});
+
+app.get("/events", (req, res) => {
+    res.send(events);
+});
 
 app.listen(4005, () => {
     console.log("Listening on port 4005")
-})
+});
